2290fcab1f6ef8340e30acf296fd4a18b2f55215
Sandro Mani <manisandro@gmail.com>
2016-12-06 19:21:03 +0100

    Port to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,83 @@
-var path = require("path");
-var DefinePlugin = require("webpack/lib/DefinePlugin");
-var NormalModuleReplacementPlugin = require("webpack/lib/NormalModuleReplacementPlugin");
-var NoErrorsPlugin = require("webpack/lib/NoErrorsPlugin");
+const webpack = require('webpack');
+const path = require('path');
 
-var rewriteUrl = function(replacePath) {
-    return function(req, opt) {  // gets called with request and proxy object
-        var queryIndex = req.url.indexOf('?');
-        var query = queryIndex >= 0 ? req.url.substr(queryIndex) : "";
-        req.url = req.path.replace(opt.path, replacePath) + query;
-    };
-};
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = nodeEnv === 'production';
+
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+  }),
+  new webpack.NamedModulesPlugin(),
+  new webpack.DefinePlugin({
+    "__DEVTOOLS__": !isProd
+  }),
+  new webpack.NormalModuleReplacementPlugin(/leaflet$/, path.join(__dirname, "qwc2", "MapStore2", "web", "client", "libs", "leaflet")),
+  new webpack.NormalModuleReplacementPlugin(/openlayers$/, path.join(__dirname, "qwc2", "MapStore2", "web", "client", "libs", "openlayers")),
+  new webpack.NormalModuleReplacementPlugin(/proj4$/, path.join(__dirname, "qwc2", "MapStore2", "web", "client", "libs", "proj4")),
+  new webpack.NoErrorsPlugin(),
+  new webpack.LoaderOptionsPlugin({
+      debug: !isProd,
+      minimize: isProd
+  })
+];
+
+if (isProd) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false
+      },
+    })
+  );
+} else {
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+}
 
 module.exports = {
-    entry: {
-        'webpack-dev-server': 'webpack-dev-server/client?http://0.0.0.0:8081', // WebpackDevServer host and port
-        'webpack': 'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-        'QWC2App': path.join(__dirname, "js", "app")
-    },
-    output: {
-        path: path.join(__dirname, "dist"),
-        publicPath: "/dist/",
-        filename: "[name].js"
-    },
-    plugins: [
-        new DefinePlugin({
-            "__DEVTOOLS__": true
-        }),
-        new NormalModuleReplacementPlugin(/leaflet$/, path.join(__dirname, "qwc2", "MapStore2", "web", "client", "libs", "leaflet")),
-        new NormalModuleReplacementPlugin(/openlayers$/, path.join(__dirname, "qwc2", "MapStore2", "web", "client", "libs", "openlayers")),
-        new NormalModuleReplacementPlugin(/proj4$/, path.join(__dirname, "qwc2", "MapStore2", "web", "client", "libs", "proj4")),
-        new NoErrorsPlugin()
-    ],
-    resolve: {
-      extensions: ["", ".js", ".jsx"]
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: 'style!css'},
-            { test: /\.less$/, loader: "style!css!less-loader" },
-            { test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
-            { test: /\.(png|jpg|gif)$/, loader: 'url-loader?name=[path][name].[ext]&limit=8192'}, // inline base64 URLs for <=8k images, direct URLs for the rest
-            {
-                test: /\.jsx?$/,
-                exclude: /ol\.js$/,
-                loader: "react-hot",
-                include: [path.join(__dirname, "js"), path.join(__dirname, "qwc2", "QWC2Components"), path.join(__dirname, "qwc2", "MapStore2", "web", "client")]
-            }, {
-                test: /\.jsx?$/,
-                exclude: /ol\.js$/,
-                loader: "babel-loader",
-                include: [path.join(__dirname, "js"), path.join(__dirname, "qwc2", "QWC2Components"), path.join(__dirname, "qwc2", "MapStore2", "web", "client")]
-            }
-        ]
-    },
-    devServer: {
-        proxy: [{
-            path: new RegExp("/mapstore/rest/geostore/(.*)"),
-            rewrite: rewriteUrl("/geostore/rest/$1"),
-            host: "mapstore.geo-solutions.it",
-            target: "http://mapstore.geo-solutions.it"
-        }, {
-            path: new RegExp("/mapstore/proxy(.*)"),
-            rewrite: rewriteUrl("/http_proxy/proxy$1"),
-            host: "localhost",
-            target: "http://localhost:8080"
-        }]
-    },
-
-    devtool: 'eval',
-    debug: true
+  devtool: isProd ? 'source-map' : 'eval',
+  entry: {
+    'webpack-dev-server': 'webpack-dev-server/client?http://0.0.0.0:8081',
+    'webpack': 'webpack/hot/only-dev-server',
+    'QWC2App': path.join(__dirname, "js", "app")
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    publicPath: "/dist/",
+    filename: '[name].js'
+  },
+  plugins,
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, use: 'file-loader', query: {name: '[name].[ext]'} },
+      { test: /\.(png|jpg|gif)$/, use: 'url-loader', query: {name: '[path][name].[ext]', limit: 8192} }, // inline base64 URLs for <=8k images, direct URLs for the rest
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'react-hot-loader',
+        include: [path.join(__dirname, "js"), path.join(__dirname, "qwc2", "QWC2Components"), path.join(__dirname, "qwc2", "MapStore2", "web", "client")]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
 };
