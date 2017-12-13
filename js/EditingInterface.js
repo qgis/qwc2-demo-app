bc40d92fb21ecbdf54d325c53ae33de09566c3a2
Sandro Mani <manisandro@gmail.com>
2017-12-13 18:56:02 +0100

    Update editing interface

--- js/EditingInterface.js
+++ js/EditingInterface.js
@@ -8,25 +8,25 @@
 
 function getFeature(layerId, mapPos, mapCrs, callback) {
     console.log("Pick " + layerId + " at (" + mapPos.x + ", " + mapPos.y + "): " + mapCrs);
+    let pos = CoordinatesUtils.reproject(mapPos, mapCrs, "EPSG:2056");
     let feature = null;
     setTimeout(() => callback(feature), 500);
 }
 
-function addFeature(layerId, feature, callback) {
+function addFeature(layerId, feature, mapCrs, callback) {
     console.log("Add to layer " + layerId + ":");
     console.log(feature);
     setTimeout(() => callback(true), 500);
 }
 
-function editFeature(layerId, feature, callback) {
+function editFeature(layerId, feature, mapCrs, callback) {
     console.log("Commit to layer " + layerId + ":");
     console.log(feature);
     setTimeout(() => callback(true), 500);
 }
 
-function deleteFeature(layerId, feature, callback) {
-    console.log("Delete feature from layer " + layerId + ":");
-    console.log(feature);
+function deleteFeature(layerId, featureId, callback) {
+    console.log("Delete feature from layer " + layerId + ":" + featureId);
     setTimeout(() => callback(true), 500);
 }
 
