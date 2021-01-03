ed33932b446fe8aedfa2e9883bdfe8832567edd5
Sandro Mani <manisandro@gmail.com>
2021-01-03 16:28:36 +0100

    Update copyright headers

--- js/CustomAttributeCalculator.jsx
+++ js/CustomAttributeCalculator.jsx
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017, Sourcepole AG.
+ * Copyright 2017-2021 Sourcepole AG
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
--- js/Help.jsx
+++ js/Help.jsx
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016, Sourcepole AG.
+ * Copyright 2016-2021 Sourcepole AG
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
