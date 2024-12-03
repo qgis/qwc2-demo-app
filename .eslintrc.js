3d4b6be6d1824b5e6a4291a37cdf1bf298cf8a40
Sandro Mani <manisandro@gmail.com>
2024-12-03 18:53:13 +0100

    Eslint: Don't flag unused args

--- .eslintrc.js
+++ .eslintrc.js
@@ -71,7 +71,7 @@ module.exports = {
         "no-shadow-restricted-names": 2, // http://eslint.org/docs/rules/no-shadow-restricted-names
         "no-unused-vars": [2, {          // http://eslint.org/docs/rules/no-unused-vars
             vars: "local",
-            args: "after-used"
+            args: "none"
         }],
         "no-use-before-define": 2,       // http://eslint.org/docs/rules/no-use-before-define
 
